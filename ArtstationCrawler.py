c0ceab13dd3cace1e896c32c466089574f293bf5
gofaquan <2660796265@qq.com>
2022-04-12 17:09:32 +0800

    :sparkles: 改用requests库

--- ArtstationCrawler.py
+++ ArtstationCrawler.py
@@ -1,11 +1,12 @@
 import json
 # 引入网络请求库
-from urllib import request
+# from urllib import request
+import requests
 
 # 地址
 url_component1 = "https://www.artstation.com/api/v2/community/explore/projects/trending.json?page="
 url_page = 1
-url_component2 = "&dimension=all&per_page=100"
+url_component2 = "&dimension=all&per_page=10"
 component1 = "https://www.artstation.com/projects/"
 component2 = ".json"
 
@@ -18,20 +19,29 @@ component2 = ".json"
 
 
 # 添加UA标识讲这个爬虫程序伪装成浏览器访问
-user_agent = "User-Agent"
-user_agent_value = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36"
+headers = {"Connection": "keep-alive",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+           "Upgrade-Insecure-Requests": "1",
+           "DNT": "1",
+           "Accept-Language": "zh-CN,zh;q=0.8",
+           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"}
 
-while url_page <= 500:
+
+while url_page <= 1:
     url = url_component1 + str(url_page) + url_component2
+    # url = "https://www.baidu.com"
     print(url)
+
+    # 发起网络请求，获取到返回的html
+    resp = requests.get(url,headers=headers,timeout=1)
+    print(resp)
+
+
+
+    # 网页返回的数据
+    # raw_json = resp.json()
+    #
+    # content = json.loads(raw_json)
+    # print(content)
+
     url_page += 1
-# # 发起网络请求，获取到返回的html
-# resp = request.Request(url)
-# # 模拟浏览器
-# resp.add_header(user_agent, user_agent_value)
-# resp = request.urlopen(resp)
-# # 网页返回的数据
-# raw_json = resp.read().decode("utf-8")
-#
-# content = json.loads(raw_json)
-# print(content['data'][0])
